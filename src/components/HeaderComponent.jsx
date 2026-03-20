9f782403e79bed00d50ac627377eff67e44b8cfb
Henrique <henriquelucio95@hotmail.com>
2026-03-20 16:29:18 -0300

    Initial Commit

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,8 @@
+export default function HeaderComponent(){
+    return (
+        <header>
+            <img src="/src/assets/Globe.svg" alt="Globe Icon"/>
+            <h1>My Travel Journal</h1>
+        </header>
+    )
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client"
+import AppComponent from "/src/components/AppComponent"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <AppComponent />
+)
